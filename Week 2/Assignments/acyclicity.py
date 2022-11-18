d7bb7176d564a191dbc11b0d765f3de7c927e4ba
Arash Latifi <arashlatifi46@gmail.com>
2022-11-18 17:25:55 +0330

    acyclicity.py Finished

--- Week 2/Assignments/acyclicity.py
+++ Week 2/Assignments/acyclicity.py
@@ -2,8 +2,23 @@
 
 import sys
 
+def explore(adj, visited, v):
+    visited[v] = -1
+    for i in adj[v]:
+        if visited[i] == 0:
+            if explore(adj, visited, i):
+                return 1
+        elif visited[i] == -1:
+            return 1
+    visited[v] = 1
 
 def acyclic(adj):
+    cc = 0
+    visited = n * [0]
+    for idx, i in enumerate(visited):
+        if visited[idx] == 0:
+            if explore(adj, visited, idx):
+                return 1
     return 0
 
 if __name__ == '__main__':
