0383d702489e60b70e70c91e69ad7b3cb581ac9a
Arash Latifi <arashlatifi46@gmail.com>
2022-11-19 03:33:01 +0330

    reverse graph function added,

--- Week 2/Assignments/strongly_connected.py
+++ Week 2/Assignments/strongly_connected.py
@@ -4,10 +4,37 @@ import sys
 
 sys.setrecursionlimit(200000)
 
+def cal_reverse(graph):
+    gr = [[] for _ in graph]
+    for i, l in enumerate(graph):
+        for x in l:
+            gr[x].append(i)
+    return gr
+
+def dfs(adj, visited, order, node):
+    if not visited[node]:
+        visited[node] = 1
+        for neighbour in adj[node]:
+            dfs(adj, visited, order, neighbour)
+        order.append(node)
+
 
 def number_of_strongly_connected_components(adj):
+    visited = [0] * n
+    order = []
+    # DFS and finding post numbers
+    for idx, i in enumerate(visited):
+        if visited[idx] == 0:
+            dfs(cal_reverse(adj), visited, order, idx)
+    order.reverse()
+    # SCCs
     result = 0
-    #write your code here
+    visited2 = [0] * n
+    temp = []
+    for idx, i in enumerate(order):
+        if visited2[i] == 0:
+            result += 1
+            dfs(adj, visited2, temp, i)
     return result
 
 if __name__ == '__main__':
