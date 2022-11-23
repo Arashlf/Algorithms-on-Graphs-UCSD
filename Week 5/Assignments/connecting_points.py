ffc60c79814018ce2e0cba9302cdc06f780dc6d9
Arash Latifi <arashlatifi46@gmail.com>
2022-11-23 18:34:02 +0330

    connecting_points.py Finished

--- Week 5/Assignments/connecting_points.py
+++ Week 5/Assignments/connecting_points.py
@@ -1,11 +1,27 @@
 #Uses python3
 import sys
-import math
+
+def prim(adj):
+    cost = n * [1e5]
+    # parent = n * [-1]
+    cost[0] = 0
+    PrioQ = ({v : cost[v] for v in range(n)})
+    while PrioQ:
+        v = min(PrioQ, key=PrioQ.get)
+        del PrioQ[v]
+        for idx, z in enumerate(adj[v]):
+            if cost[idx] > z and idx in PrioQ:
+                cost[idx] = z
+                # parent[idx] = v
+                PrioQ[idx] = cost[idx]
+    return cost
 
 def minimum_distance(x, y):
     result = 0.
-    #write your code here
-    return result
+    V = [v for v in range(n)]
+    adj = [[((x[i] - x[j])**2 + (y[i] - y[j])**2)**0.5 for i in range(n)] for j in range(n)]
+    result = prim(adj)
+    return sum(result)
 
 
 if __name__ == '__main__':
